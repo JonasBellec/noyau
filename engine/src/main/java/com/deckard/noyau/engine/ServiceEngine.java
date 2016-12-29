010e75c6cc2ad407f978335a8df1984d22baf036
Jonasjonas.bellec@gmail.com <Jonasjonas.bellec@gmail.com>
2016-12-29 15:59:06 +0100

    Résolution des problèmes de conf' Maven / JBoss

--- engine/src/main/java/com/deckard/noyau/engine/ServiceEngine.java
+++ engine/src/main/java/com/deckard/noyau/engine/ServiceEngine.java
@@ -7,16 +7,9 @@ import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.ws.rs.NotSupportedException;
-
-import org.omg.CORBA.SystemException;
 
 import com.deckard.noyau.core.dao.administration.WarehouseAdministration;
 import com.deckard.noyau.core.dao.dungeon.WarehouseDungeon;
-import com.deckard.noyau.core.dao.instance.HeuristicMixedException;
-import com.deckard.noyau.core.dao.instance.HeuristicRollbackException;
-import com.deckard.noyau.core.dao.instance.RollbackException;
-import com.deckard.noyau.core.dao.instance.TransactionManager;
 import com.deckard.noyau.core.dao.instance.WarehouseInstance;
 import com.deckard.noyau.core.dao.request.WarehouseRequest;
 import com.deckard.noyau.core.http.AbstractService;
@@ -34,13 +27,13 @@ public class ServiceEngine extends AbstractService {
 	ExecutorService service = Executors.newFixedThreadPool(10);
 
 	@Inject
+	private WarehouseAdministration warehouseAdministration;
+
+	@Inject
 	private WarehouseRequest warehouseRequest;
 
 	@Inject
 	private WarehouseInstance warehouseInstance;
-
-	@Inject
-	private WarehouseAdministration warehouseAdministration;
 
 	@Inject
 	private WarehouseDungeon warehouseDungeon;
@@ -55,7 +48,6 @@ public class ServiceEngine extends AbstractService {
 
 	public Result stop() {
 		scheduledExecutorServiceMonitoring.shutdown();
-
 		return createResultNoElement(HttpCode.OK);
 	}
 
@@ -79,47 +71,19 @@ public class ServiceEngine extends AbstractService {
 		}
 
 		public Instance createInstance(Player player, Dungeon dungeon) {
-			Instance instance = null;
-			TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
-			try {
-				tm.begin();
 
-				instance = new Instance();
+			instance = new Instance();
 
-				instance.setIdDungeon(dungeon.getId());
+			instance.setIdDungeon(dungeon.getId());
 
-				Persona persona = new Persona();
-				persona.setIdPlayer(player.getId());
-				persona.setX(0);
-				persona.setY(0);
+			Persona persona = new Persona();
+			persona.setIdPlayer(player.getId());
+			persona.setX(0);
+			persona.setY(0);
 
-				instance.getListPersona().add(persona);
+			instance.getListPersona().add(persona);
 
-				entityManagerInstance.persist(instance);
-
-				tm.commit();
-			} catch (NotSupportedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SystemException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (HeuristicMixedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (HeuristicRollbackException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (RollbackException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			entityManagerInstance.persist(instance);
 
 			return instance;
 		}
